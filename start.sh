af42a755ef79af5df6135bd228502190204ac1d1
axizzy19 <137169420+axizzy19@users.noreply.github.com>
2026-07-18 21:28:00 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,15 +3,12 @@
 # Переходим в папку бэкенда
 cd /app/backend
 
-# Устанавливаем uvicorn (если не установлен)
-pip install uvicorn fastapi -q
-
 # Запускаем бэкенд
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
-# Ждём, пока бэкенд запустится
+# Ждём запуск бэкенда
 sleep 5
 
-# Переходим в папку с фронтендом и запускаем простой сервер
+# Запускаем фронтенд (статику)
 cd /app/frontend/build
 python3 -m http.server 3000
